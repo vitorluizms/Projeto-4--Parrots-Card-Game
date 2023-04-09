e3acb8a1363681f6e9c962c4cf982054ebea12f1
Vitor Luiz <vitor.luiz.eer@gmail.com>
2023-04-09 13:29:16 -0300

    Função de virar cartas e finalizar jogo adicionadas

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const imagens_frente = ["imagens/bobrossparrot.gif", "imagens/explodyparrot.gif", "imagens/fiestaparrot.gif", "imagens/metalparrot.gif", 
-    "imagens/revertitparrot.gif", "imagens/tripletsparrot.gif", "imagens/unicornparrot.gif", ];
+const imagens = ["imagens/bobrossparrot.gif", "imagens/explodyparrot.gif", "imagens/fiestaparrot.gif", "imagens/metalparrot.gif", 
+    "imagens/revertitparrot.gif", "imagens/tripletsparrot.gif", "imagens/unicornparrot.gif"]
 
 let qtde;
 let elemento1;
@@ -11,41 +11,96 @@ function comparador () {
     return Math.random() -0.5;
 }
 
-function qtde_cartas() {
+function qtdeCartas() {
     let gerarCartas;
-    gerarCartas = document.querySelector(".containter-cartas")
+    gerarCartas = document.querySelector(".container-carta")
     gerarCartas.innerHTML = ' ';
     contador = 0;
     limite = 2;
     imagens.sort(comparador)
 
     const ListaDeImagens = []
-
-    qtde = Number(prompt("Escolha a quantidade de cartas (4 a 14)"))
+    qtde = Number(prompt("Escolha a quantidade de cartas(4 a 14"))
     while (qtde % 2 != 0 || qtde > 14 || qtde < 4) {
-        qtde = Number(prompt("Escolha a quantidade de cartas (4 a 14)"))
+        qtde = prompt("Escolha a quantidade de cartas (4 a 14)")
     } 
 
-    let qtde_cartas = imagens_frente.slice(0, qtde/2)
+    let qtd_cartas = imagens.slice(0, qtde/2)
 
-    for (i = 0; i < qtde_cartas.length; i++) {
-        ListaDeImagens.push(qtde_cartas[i]);
-        ListaDeImagens.push(qtde_cartas[i]);
+    for (i = 0; i < qtd_cartas.length; i++) {
+        ListaDeImagens.push(qtd_cartas[i]);
+        ListaDeImagens.push(qtd_cartas[i]);
     }
 
     ListaDeImagens.sort(comparador);
     for (let i = 0; i < qtde; i++) {
-        gerarCartas = document.querySelector(".container-cartas")
+        gerarCartas = document.querySelector(".container-carta")
         gerarCartas.innerHTML += ` 
-                <div class="card" onclick = "virarCartas(this)">
+            <div class="carta" onclick = "virarCartas(this)">
                 <div class="front-face face">
-                <img src="imagens/front.png" alt="">
+                    <img src="imagens/back.png" alt="">
                 </div>
                 <div class="back-face face">
-                <img src=${ListadeImagens[i]}>
+                    <img src=${ListaDeImagens[i]}>
                 </div>
             </div>`
-
     }
 }
 
+function virarCartas(elemento) {
+    if (elemento.classList.contains("to-turn")) {
+        return
+    }
+    if (document.querySelectorAll(".carta.to-turn").length >= limite){
+        return
+    }
+
+    elemento.classList.add("to-turn")
+
+    let CartasViradas = document.querySelectorAll(".to-turn")
+    contador++
+    if ((CartasViradas.length)%2 !==0) {
+        elemento1 = elemento;
+        imagem1 = elemento1.querySelector(".back-face img").src
+    }
+
+    if ((CartasViradas.length)%2 === 0) {
+        let imagem2 = elemento.querySelector(".back-face img").src
+        CompararCartas(imagem1, imagem2, elemento1, elemento)
+    }
+
+    function CompararCartas(imagem1, imagem2, elemento1, elemento) {
+        if (imagem1 != imagem2) {
+            setTimeout(function remover(){elemento.classList.remove('to-turn')
+            elemento1.classList.remove("to-turn")} , 1000);
+
+        }
+        if (imagem1 == imagem2) {
+            limite += 2
+        }
+        setTimeout(FinalizarPartida , 1050)
+    }
+}
+
+function FinalizarPartida() {
+    let qtdeToTurn = document.querySelectorAll(".carta.to-turn")
+    if (qtdeToTurn.length == qtde) {
+        alert(`Você ganhou em ${contador} jogadas!`)
+        reiniciarPartida()
+    }
+}
+
+function reiniciarPartida(){
+    let resposta = prompt("Quer reiniciar a partida? (sim ou não)")
+    while (resposta !== 'não' && resposta !== 'sim') {
+        resposta= prompt("Quer reiniciar a partida? (sim ou não)")
+    }
+    if (resposta === 'sim') {
+        qtdeCartas()
+    }
+    if (resposta === 'não') {
+        alert("Ok")
+    }
+}
+
+qtdeCartas()
