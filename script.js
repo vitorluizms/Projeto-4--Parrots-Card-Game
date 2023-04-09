bee0bbfda2d0bda03d1a09418e2c937480027777
Vitor Luiz <vitor.luiz.eer@gmail.com>
2023-04-09 11:56:01 -0300

    função de gerar cartas adicionada

--- script.js
+++ script.js
@@ -1,10 +1,51 @@
-const cartas = [];
+const imagens_frente = ["imagens/bobrossparrot.gif", "imagens/explodyparrot.gif", "imagens/fiestaparrot.gif", "imagens/metalparrot.gif", 
+    "imagens/revertitparrot.gif", "imagens/tripletsparrot.gif", "imagens/unicornparrot.gif", ];
 
-const qtde = prompt('Digite a quantidade de tarefas que você quer');
-
-const container_card = document.querySelector('.container-carta');
+let qtde;
+let elemento1;
+let imagem1;
 let contador = 0;
-while (qtde > contador) {
-    container_card.innerHTML += `<button class = carta><img src="./imagens/back.png" alt="back"/></button>`
-    contador++;
+let limite;
+
+function comparador () {
+    return Math.random() -0.5;
 }
+
+function qtde_cartas() {
+    let gerarCartas;
+    gerarCartas = document.querySelector(".containter-cartas")
+    gerarCartas.innerHTML = ' ';
+    contador = 0;
+    limite = 2;
+    imagens.sort(comparador)
+
+    const ListaDeImagens = []
+
+    qtde = Number(prompt("Escolha a quantidade de cartas (4 a 14)"))
+    while (qtde % 2 != 0 || qtde > 14 || qtde < 4) {
+        qtde = Number(prompt("Escolha a quantidade de cartas (4 a 14)"))
+    } 
+
+    let qtde_cartas = imagens_frente.slice(0, qtde/2)
+
+    for (i = 0; i < qtde_cartas.length; i++) {
+        ListaDeImagens.push(qtde_cartas[i]);
+        ListaDeImagens.push(qtde_cartas[i]);
+    }
+
+    ListaDeImagens.sort(comparador);
+    for (let i = 0; i < qtde; i++) {
+        gerarCartas = document.querySelector(".container-cartas")
+        gerarCartas.innerHTML += ` 
+                <div class="card" onclick = "virarCartas(this)">
+                <div class="front-face face">
+                <img src="imagens/front.png" alt="">
+                </div>
+                <div class="back-face face">
+                <img src=${ListadeImagens[i]}>
+                </div>
+            </div>`
+
+    }
+}
+
